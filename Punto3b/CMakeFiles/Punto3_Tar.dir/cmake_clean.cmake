fb3f046398ead7d0941bd3c6d31774e09a15541d
StivenZar <60946020+StivenZar@users.noreply.github.com>
2020-03-22 18:33:56 -0500

    Add files via upload

---
+++ Punto3b/CMakeFiles/Punto3_Tar.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "Punto3_Tar_autogen"
+  "CMakeFiles/Punto3_Tar_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/Punto3_Tar.dir/main.cpp.o"
+  "CMakeFiles/Punto3_Tar.dir/Polinomio.cpp.o"
+  "CMakeFiles/Punto3_Tar.dir/Punto3_Tar_autogen/mocs_compilation.cpp.o"
+  "Punto3_Tar.pdb"
+  "Punto3_Tar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Punto3_Tar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Punto3b/CMakeFiles/Punto3_Tar_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Punto3_Tar_autogen"
+  "CMakeFiles/Punto3_Tar_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/Punto3_Tar_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Punto3_Tar_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
