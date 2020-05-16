4549316c32d501ed9840417dcd6805b377933abf
StivenZar <edward.zarate@mail.escuelaing.edu.co>
2020-05-15 19:22:27 -0500

    flujo hecho 0.0

--- Proyecto_definitivo_3.0/main.cpp
+++ Proyecto_definitivo_3.0/main.cpp
@@ -6,8 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     opciones_deusr b;
-    datosentrada_sesion_usr c;
-    c.show();
-    c.exec()
+    //datosentrada_sesion_usr c;
+    b.show();
     return a.exec();
 }
